d2272afa87f9bc2fbf1ee6209e91b12c294c3145
SerhiiOleskiv <oleskiv00@gmail.com>
2023-02-09 19:52:12 +0200

    update 2

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Form = () => {
+    return (
+        <form>
+            <div className="form-group">
+                <input type="text"
+                className="form-control"
+                       placeholder="Type name of the note"
+                />
+            </div>
+        </form>
+    )
+}
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,10 @@
 import React, {Fragment} from "react";
 
-export const About = () => {
-    return (
-        <Fragment>
-            <h1>About page</h1>
-        </Fragment>
-
+export const About = () => (
+    <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+            <h1 className="display-4">React To-do App</h1>
+            <p className="lead">First tries to make app on React JS</p>
+        </div>
+    </div>
     )
-}
